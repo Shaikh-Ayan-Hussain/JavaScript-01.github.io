07d118d0d95dbe9cc5374f3d097ca93db6ece9f8
Shaikh Ayan <ayanhussain2132006gmail.com>
2025-01-18 16:09:05 +0500

    Complete

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,4 @@ weight_in_grams =parseFloat(weight_in_grams);
 
 const total_price_per_chicken = (weight_in_grams * price_per_kg) / 1000 ; 
 
-alert(`You have to pay ${total_price_per_chicken} for ${weight_in_grams}gm of chicken`);
+document.write(`You have to pay ${total_price_per_chicken} for ${weight_in_grams}gm of chicken`);
