d10eca4f8b3a48d17091f6730de3cb7839d19508
Shaikh Ayan <ayanhussain2132006gmail.com>
2025-01-18 16:11:02 +0500

    DONE

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 const price_per_kg= 500;
 
-var  weight_in_grams= prompt("How much chicken do you want to");
-
-weight_in_grams =parseFloat(weight_in_grams);
+var  weight_in_grams= parseFloat(prompt("How much chicken do you want to"));
 
 const total_price_per_chicken = (weight_in_grams * price_per_kg) / 1000 ; 
 
